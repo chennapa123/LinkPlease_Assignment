e165968a44d2ed269db9815649b8e3935a8a3d26
chennapa123 <chennapanaidu_cherukuri@srmap.edu.in>
2026-08-17 22:30:59 +0530

    database update

--- app/database.py
+++ app/database.py
@@ -1,10 +1,8 @@
+# app/database.py
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 from app.config import get_settings
-
-# Import models to register SQLAlchemy metadata before schema creation.
-# from app import models  # noqa: F401
 
 settings = get_settings()
 engine = create_engine(settings.database_url, pool_pre_ping=True)
